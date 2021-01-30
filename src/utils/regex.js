47b5e0a4bd7acad0f91cc17457108990df69dd37
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-01-29 20:00:32 -0800

    Refactored

--- src/utils/regex.js
+++ src/utils/regex.js
@@ -1,7 +1,7 @@
-const regexAddress = /^[A-Z0-9 ,#'\/.]{3,96}$/iu;
+const regexAddress = /^[A-Z0-9 ,#'/.]{3,96}$/iu;
 const regexEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 const regexName = /^[a-zA-Z]{2}(([' -][a-zA-Z ])?[a-zA-Z]*)*$/;
-const regexPhone = /[\(]\d{3}[\)]\s?\d{3}[\-]\d{4}/;
+const regexPhone = /[(]\d{3}[)]\s?\d{3}[-]\d{4}/;
 const regexPhoto = /^\b(jpeg|jpg|png)\b$/;
 
 export { regexAddress, regexEmail, regexName, regexPhone, regexPhoto };
