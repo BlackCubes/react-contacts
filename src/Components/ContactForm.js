38da4721c5be8820963fc7b1fcb6e0a4c2371d07
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-01-31 18:11:13 -0800

    Consoled log for debugging

--- src/Components/ContactForm.js
+++ src/Components/ContactForm.js
@@ -83,6 +83,7 @@ class ContactForm extends React.Component {
 
   handleFileChange(e) {
     const file = e.target.files[0];
+    console.log(file);
     const reader = new FileReader();
     reader.readAsDataURL(file);
     reader.onloadend = () => this.setState({ fileUpload: reader.result });
