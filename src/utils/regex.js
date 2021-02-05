e94abbf598f1fd9f4686b5b9c2813d0b51db2411
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-02-04 19:00:27 -0800

    Created two regex's for username and password

--- src/utils/regex.js
+++ src/utils/regex.js
@@ -1,7 +1,17 @@
 const regexAddress = /^[A-Z0-9 ,#'/.]{3,96}$/iu;
 const regexEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 const regexName = /^[a-zA-Z]{2}(([' -][a-zA-Z ])?[a-zA-Z]*)*$/;
+const regexPass = /^(?=.*?[0-9])(?=.*?[A-Z])(?=.*?[.#?!@$%^&*\\-_]).{8,60}$/;
 const regexPhone = /[(]\d{3}[)]\s?\d{3}[-]\d{4}/;
 const regexPhoto = /^\b(jpeg|jpg|png)\b$/;
+const regexUsername = /^(?!.*[-_]{2,})(?=^[^-_].*[^-_]$)[\w\s-]{3,9}$/;
 
-export { regexAddress, regexEmail, regexName, regexPhone, regexPhoto };
+export {
+  regexAddress,
+  regexEmail,
+  regexName,
+  regexPass,
+  regexPhone,
+  regexPhoto,
+  regexUsername,
+};
