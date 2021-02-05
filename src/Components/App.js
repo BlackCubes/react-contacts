c7ff7544b986b5ff30ae734688198ab39c4ba36d
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-02-04 20:21:24 -0800

    Commented out and refactored it

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './App.css';
 
-// import ContactForm from './ContactForm';
-// import ContactList from './ContactList';
-import LoginForm from './LoginForm';
+import ContactForm from './ContactForm';
+import ContactList from './ContactList';
+// import LoginForm from './LoginForm';
 
 import AuthProvider from '../providers/AuthProvider';
-// import ContactProvider from '../providers/ContactProvider';
+import ContactProvider from '../providers/ContactProvider';
 
 function App() {
   // const [contacts, setContacts] = useState([]);
@@ -19,15 +19,15 @@ function App() {
   return (
     <>
       <AuthProvider>
-        <LoginForm />
-        {/* <ContactProvider>
+        {/* <LoginForm /> */}
+        <ContactProvider>
           <div className="contact-form--wrapper">
             <ContactForm />
           </div>
           <div className="contact-list--wrapper">
             <ContactList />
           </div>
-        </ContactProvider> */}
+        </ContactProvider>
       </AuthProvider>
     </>
   );
