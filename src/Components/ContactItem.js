3558d08b8c9eea02e18375af4a8e334ba9ac9890
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-02-04 20:28:47 -0800

    Fixed syntax error

--- src/Components/ContactItem.js
+++ src/Components/ContactItem.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import useContactContext from '../hooks/index';
+import { useContactContext } from '../hooks/index';
 
 const ContactItem = ({
   firstName,
