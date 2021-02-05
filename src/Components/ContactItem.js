eaac15bd6c565b51870b2193733a24ae9ef0f55f
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-02-04 20:28:08 -0800

    Added return statement

--- src/Components/ContactItem.js
+++ src/Components/ContactItem.js
@@ -12,31 +12,33 @@ const ContactItem = ({
 }) => {
   const { deleteContact } = useContactContext();
 
-  <div className="contact-item">
-    <div className="contact-item__profile">
-      <img
-        className="contact-item__profile-img"
-        src={profileImg}
-        alt="User Profile"
-      />
-    </div>
-
-    <div className="contact-item__info">
-      <div className="contact-item__info-name">
-        <span>{`${firstName} ${lastName}`}</span>
+  return (
+    <div className="contact-item">
+      <div className="contact-item__profile">
+        <img
+          className="contact-item__profile-img"
+          src={profileImg}
+          alt="User Profile"
+        />
       </div>
 
-      <div className="contact-item__info-phone">
-        <span>{phoneNumber}</span>
+      <div className="contact-item__info">
+        <div className="contact-item__info-name">
+          <span>{`${firstName} ${lastName}`}</span>
+        </div>
+
+        <div className="contact-item__info-phone">
+          <span>{phoneNumber}</span>
+        </div>
+      </div>
+
+      <div className="contact-item__close">
+        <button type="button" onClick={deleteContact(index)}>
+          &times;
+        </button>
       </div>
     </div>
-
-    <div className="contact-item__close">
-      <button type="button" onClick={deleteContact(index)}>
-        &times;
-      </button>
-    </div>
-  </div>;
+  );
 };
 
 ContactItem.propTypes = {
