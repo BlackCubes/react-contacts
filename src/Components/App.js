a7f0d50eb046bf743c4078723d595476078b373e
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-02-07 19:39:20 -0800

    Implemented react-router-dom

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 import './App.css';
 
 import ContactForm from './ContactForm';
@@ -14,12 +15,16 @@ function App() {
       <AuthProvider>
         {/* <LoginForm /> */}
         <ContactProvider>
-          <div className="contact-form--wrapper">
+          <Switch>
+            <Route exact path="/" component={ContactList} />
+            <Route exact path="/contacts/add" component={ContactForm} />
+          </Switch>
+          {/* <div className="contact-form--wrapper">
             <ContactForm />
           </div>
           <div className="contact-list--wrapper">
             <ContactList />
-          </div>
+          </div> */}
         </ContactProvider>
       </AuthProvider>
     </>
