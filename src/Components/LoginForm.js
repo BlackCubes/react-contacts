800cff61b081b88d0608f2d993e92b1e8a227f0a
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-02-09 12:35:02 -0800

    Expanded with more validation and custom classes

--- src/Components/LoginForm.js
+++ src/Components/LoginForm.js
@@ -2,54 +2,122 @@ import React, { useState } from 'react';
 
 import { useAuthContext } from '../hooks/index';
 
+import * as regex from '../utils/regex';
+
 const LoginForm = () => {
   const [values, setValues] = useState({});
+  const [errors, setErrors] = useState({});
   const { login } = useAuthContext();
+
+  const validateForm = (errorList) => {
+    let valid = true;
+    Object.values(errorList).forEach((err) => {
+      if (err.length) valid = false;
+    });
+    return valid;
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-
-    login(values.username, values.password);
+    if (validateForm(errors)) login(values.username, values.password);
     setValues({});
   };
 
   const handleChange = (e) => {
     const { name, value } = e.target;
 
+    switch (name) {
+      case 'username':
+        if (!value.length)
+          setErrors((err) => ({
+            ...err,
+            [name]: 'Required.',
+          }));
+        else if (value.length < 3)
+          setErrors((err) => ({
+            ...err,
+            [name]: 'Must be a minimum of 3 characters.',
+          }));
+        else if (value.length > 9)
+          setErrors((err) => ({
+            ...err,
+            [name]: 'Must be less than or equal to 9 characters.',
+          }));
+        else if (!regex.regexUsername(value))
+          setErrors((err) => ({
+            ...err,
+            [name]:
+              'Must use at between 3 and 9 characters with optional underscores and hypens, and all lowercase.',
+          }));
+        else setErrors((err) => ({ ...err, [name]: '' }));
+        break;
+
+      case 'password':
+        if (!value.length)
+          setErrors((err) => ({
+            ...err,
+            [name]: 'Required.',
+          }));
+        else if (!regex.regexPass.test(value))
+          setErrors((err) => ({
+            ...err,
+            [name]:
+              'Must use at least one number, one special character, and on capital letter between 8 and 60 characters.',
+          }));
+        else setErrors((err) => ({ ...err, [name]: '' }));
+        break;
+      default:
+        break;
+    }
+
     setValues((val) => ({ ...val, [name]: value }));
   };
 
   return (
     <form onSubmit={handleSubmit} noValidate>
-      <label htmlFor="username">
-        <input
-          type="text"
-          name="username"
-          id="username"
-          value={values.username || ''}
-          placeholder="Username"
-          onChange={handleChange}
-          noValidate
-        />
+      <div className="form__group">
+        <label htmlFor="username">
+          <input
+            type="text"
+            name="username"
+            className="form__input input-text"
+            id="username"
+            value={values.username || ''}
+            placeholder="Username"
+            onChange={handleChange}
+            noValidate
+          />
 
-        <span>Enter username.</span>
-      </label>
+          <span className="form__label label-text">
+            {errors.username ? errors.username : 'Enter username.'}
+          </span>
+        </label>
+      </div>
 
-      <label htmlFor="password">
-        <input
-          type="password"
-          name="password"
-          id="password"
-          value={values.password || ''}
-          placeholder="Password"
-          onChange={handleChange}
-          noValidate
-        />
+      <div className="form__group">
+        <label htmlFor="password">
+          <input
+            type="password"
+            name="password"
+            className="form__input input-text"
+            id="password"
+            value={values.password || ''}
+            placeholder="Password"
+            onChange={handleChange}
+            noValidate
+          />
 
-        <span>Enter password.</span>
-      </label>
+          <span className="form__label label-text">
+            {errors.password ? errors.password : 'Enter password.'}
+          </span>
+        </label>
+      </div>
 
-      <button type="submit">Login</button>
+      <div className="form__group">
+        <button className="btn" type="submit">
+          Login
+        </button>
+      </div>
     </form>
   );
 };
