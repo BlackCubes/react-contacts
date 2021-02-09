93586746cc81b560f9ff553f186f0a94901a2cd7
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-02-09 12:59:07 -0800

    Implemented the dummy json data

--- src/providers/AuthProvider.js
+++ src/providers/AuthProvider.js
@@ -4,28 +4,31 @@ import PropTypes from 'prop-types';
 
 import AuthContext from '../context/AuthContext';
 
-import * as regex from '../utils/regex';
+import loginAPI from '../utils/login';
+// import * as regex from '../utils/regex';
 
 const AuthProvider = ({ children }) => {
   const [loggedIn, setLoggedIn] = useState(null);
   const history = useHistory();
 
-  const login = (username, password) => {
-    let valid = true;
+  const login = async (usernameInput, passwordInput) => {
+    // let valid = true;
 
-    if (!password) valid = false;
-    else if (!regex.regexPass.test(password)) valid = false;
-    else valid = true;
+    // if (!password) valid = false;
+    // else if (!regex.regexPass.test(password)) valid = false;
+    // else valid = true;
 
-    if (!username) valid = false;
-    else if (username.length < 3) valid = false;
-    else if (username.length > 9) valid = false;
-    else if (!regex.regexUsername.test(username)) valid = false;
-    else valid = true;
+    // if (!username) valid = false;
+    // else if (username.length < 3) valid = false;
+    // else if (username.length > 9) valid = false;
+    // else if (!regex.regexUsername.test(username)) valid = false;
+    // else valid = true;
+    const { username, password } = await loginAPI();
 
-    console.log(valid, username, password);
+    console.log(username, password);
 
-    if (valid) setLoggedIn(username);
+    if (usernameInput === username && passwordInput === password)
+      setLoggedIn(username);
   };
 
   const logout = () => {
